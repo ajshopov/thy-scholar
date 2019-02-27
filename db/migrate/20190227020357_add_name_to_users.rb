dce0dce389e0e9b0fb7ba046859a1454811103de
ajshopov <alexanderjshopov@gmail.com>
2019-02-27 14:06:10 +1100

    changed migration so error not thrown for missing name

--- db/migrate/20190227020357_add_name_to_users.rb
+++ db/migrate/20190227020357_add_name_to_users.rb
@@ -1,5 +1,10 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :name, :string, null: false, default: ""
+  def up
+    add_column :users, :name, :string, default: "edit profile to add your name"
+    change_column :users, :name, :string, null: false, default: ""
+  end
+
+  def down
+    remove_column :users, :name
   end
 end
