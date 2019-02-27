28250280e018327da63a32a32f0c0e3573a0ddfd
ajshopov <alexanderjshopov@gmail.com>
2019-02-27 12:53:52 +1100

    add question history to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @question = Question.new(recipient_id: @user.id)
-    # byebug
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :questions
   # has_many :answers
+
+  def is_recipient_for
+    Question.where(recipient_id: id).order(created_at: :desc)
+  end
+
+  def is_sender_for
+    Question.where(sender_id: id).order(created_at: :desc)
+  end
 end
