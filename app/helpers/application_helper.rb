e00028f5a6eec722138f008fe500245d55df9271
ajshopov <alexanderjshopov@gmail.com>
2019-03-21 13:00:53 +1100

    set default profile pic and helper for viewing profile with profile pic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def has_stripe_acct
     yield if current_user.try(:stripe_id?)
   end
+
+  def check_avatar(user)
+    if user.profile_pic.present?
+      #return user avatar
+      user.profile_pic_url(:thumb)
+    else
+      # return default image
+      "fallback/default.png"
+    end
+  end
 end
