a7d9dde1535cb6694743db68f29b7508991c3887
ajshopov <alexanderjshopov@gmail.com>
2019-04-10 09:20:38 +1000

    title added to lecture and tidy up rspec lectures

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,5 @@
 class Lecture < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
 end
--- spec/features/adding_lectures_spec.rb
+++ spec/features/adding_lectures_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Users can create new lectures' do
     scenario "user must be signed in" do
       visit user_path(user)
 
-      expect(page).to have_no_button('Create Lecture')
+      expect(page).to have_no_button('New Lecture')
     end
   end
 
@@ -18,19 +18,22 @@ RSpec.feature 'Users can create new lectures' do
       visit user_path(user)
     end
 
+    # not set up for testing content presence ~~~~
+    # can't work out how to select trix-editor to fill_in
     scenario 'with valid attributes' do
-      fill_in 'Content', with: 'This website needs more work.'
+      click_link "New Lecture"
+      fill_in 'Title', with: 'The Known Unknown'
       click_button 'Create Lecture'
 
-      expect(page).to have_content 'Lecture has been created.'
-      expect(page).to have_content "By #{user.name}"
+      expect(page).to have_content 'Lecture was successfully created.'
+      expect(page).to have_content "#{user.name}"
     end
 
     scenario 'with invalid attributes' do
+      click_link "New Lecture"
       click_button 'Create Lecture'
 
-      expect(page).to have_content 'Lecture has not been created.'
-      expect(page).to have_content "Content can't be blank"
+      expect(page).to have_content "Title can't be blank"
     end
   end
 end
