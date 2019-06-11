fd1bde9d8171681b41a5f468db51bed4c6e78c94
ajshopov <alexanderjshopov@gmail.com>
2019-06-11 20:59:11 +1000

    added helper method has_answer

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,10 @@
-# frozen_string_literal: true
-
 module QuestionsHelper
+  def has_answer(question)
+    if !question.answer.nil?
+      truncate(strip_tags(question.answer.response), length: 100,
+        separator: ' '){ link_to "(read more)", question_path(question) }
+    else
+      "This question hasnt been answered yet."
+    end
+  end
 end
